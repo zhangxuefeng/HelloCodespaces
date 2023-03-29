b9690d793f03ae732aee26dcc28e81a99a8129b9
zhangxuefeng <willian.mcs@hotmail.com>
2023-03-29 08:37:39 +0000

    just commit

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
 # check prime number function in python 3 
+def is_prime(n):
+    if n == 1:
+        return False
+    elif n == 2:
+        return True
+    else:
+        for x in range(2, n):
+            if n % x == 0:
+                return False
+        return True
 
-print('hello python')
+# print prime numbers between 1 to 100
+for n in range(1, 100):
+    if is_prime(n):
+        print(n)
+
+# generate other test cases for the function
+for n in range(1, 100):
+    if is_prime(n):
+        print('assert is_prime({}) == True'.format(n))
+    else:
+        print('assert is_prime({}) == False'.format(n))
